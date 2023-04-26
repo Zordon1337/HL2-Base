97d697a6e1b40800a1365022c701961b11ac5c3c
Zordon <65111609+Zordon1337@users.noreply.github.com>
2023-04-26 22:16:19 +0200

    Uploaded SRC

---
+++ HL2-BASE/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace HL2_BASE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialSwitch1 = new MaterialSkin.Controls.MaterialSwitch();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialSwitch1
+            // 
+            this.materialSwitch1.AutoSize = true;
+            this.materialSwitch1.Depth = 0;
+            this.materialSwitch1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.materialSwitch1.Location = new System.Drawing.Point(13, 66);
+            this.materialSwitch1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch1.Name = "materialSwitch1";
+            this.materialSwitch1.Ripple = true;
+            this.materialSwitch1.Size = new System.Drawing.Size(169, 37);
+            this.materialSwitch1.TabIndex = 0;
+            this.materialSwitch1.Text = "Auto BunnyHop";
+            this.materialSwitch1.UseVisualStyleBackColor = true;
+            this.materialSwitch1.CheckedChanged += new System.EventHandler(this.materialSwitch1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.materialLabel1);
+            this.panel1.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(265, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 226);
+            this.panel1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(229, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "D";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(124, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "SPACE";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "S";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "W";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(144, 15);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(78, 19);
+            this.materialLabel1.TabIndex = 0;
+            this.materialLabel1.Text = "Keystrokes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 297);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.materialSwitch1);
+            this.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3, 54, 3, 3);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HalfLife2 Base";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ HL2-BASE/Form1.cs
@@ -0,0 +1,172 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Runtime.InteropServices;
+using Memory;
+using HL2_BASE;
+
+namespace HL2_BASE
+{
+    
+    public partial class Form1 : MaterialForm
+    {
+        [DllImport("User32.dll")]
+        public static extern bool GetAsyncKeyState(int key);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public async Task KeyStrokesManager()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (GetAsyncKeyState(0x57))
+                    {
+                        ResetKeys("W");
+                    }
+                    if (GetAsyncKeyState(0x53))
+                    {
+                        ResetKeys("S");
+                    }
+                    if (GetAsyncKeyState(0x41))
+                    {
+                        ResetKeys("A");
+                    }
+                    if (GetAsyncKeyState(0x44))
+                    {
+                        ResetKeys("D");
+                    }
+                    if (GetAsyncKeyState(0x20))
+                    {
+                        ResetKeys("Space");
+                    }
+                }
+            });
+        }
+
+        public void ResetKeys(string Key)
+        {
+            if(Key=="W")
+            {
+                label1.ForeColor = Color.Red;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.White;
+                label4.ForeColor = Color.White;
+                label5.ForeColor = Color.White;
+            }
+            if (Key == "S")
+            {
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.Red;
+                label3.ForeColor = Color.White;
+                label4.ForeColor = Color.White;
+                label5.ForeColor = Color.White;
+            }
+            if (Key == "A")
+            {
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.Red;
+                label4.ForeColor = Color.White;
+                label5.ForeColor = Color.White;
+            }
+            if (Key == "D")
+            {
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.White;
+                label4.ForeColor = Color.Red;
+                label5.ForeColor = Color.White;
+            }
+            if (Key == "Space")
+            {
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.White;
+                label4.ForeColor = Color.White;
+                label5.ForeColor = Color.Red;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Hacks.Init();
+            Hacks.autobh();
+            KeyStrokesManager();
+
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Grey600, Primary.Green600,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+        private void materialSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            config.BHOP = !config.BHOP;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+class config
+{
+    public static bool BHOP = false;
+}
+
+class Hacks
+{
+    static Mem _mem = new Mem();
+    public static void Init() { int id = _mem.GetProcIdFromName("hl2.exe"); if (id < 0) { MessageBox.Show("Half-Life 2 Isn't running"); } else { _mem.OpenProcess(id); } }
+    [DllImport("User32.dll")]
+    public static extern bool GetAsyncKeyState(int key);
+    public static async Task autobh() { 
+        await Task.Run(() => 
+        { 
+            while (true) 
+            { 
+                if (config.BHOP) 
+                { 
+                    if (GetAsyncKeyState(0x20)) 
+                    { 
+                        _mem.WriteMemory(offsets.DwForceJump, "int", "5"); 
+                        _mem.WriteMemory(offsets.DwForceJump, "int", "4"); 
+                    } 
+                } 
+            } 
+        }); 
+    }
+    
+}
+
+class offsets
+{
+    public static string DwForceJump = "client.dll+0x04B4D24";
+    public static string DwForceAttack = "client.dll+0x04B4D30";
+    public static string DwForceAttack2 = "client.dll+0x04B4D3C";
+    public static string DwForceAttack3 = "client.dll+0x04B4DCC";
+    public static string DwForceDuck = "client.dll+0x04B4D60";
+    public static string WallHack = "client.dll+0x0483890";
+}
